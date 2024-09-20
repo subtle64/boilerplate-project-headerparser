d0494b82fbe530536ce317be168e67e6d345a0d2
Jason Adriel <143072575+subtle64@users.noreply.github.com>
2024-09-20 17:10:09 +0000

    Tests passed.

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  console.log(req)
+  res.json({
+    'ipaddress': req.ip,
+    'language': req.get('accept-language'),
+    'software': req.get('User-Agent')
+  })
 });
 
 // listen for requests :)
